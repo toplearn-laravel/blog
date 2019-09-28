43391795fe4363fb319ac65c72e7ad69f387389e
hassan khosrojerdi <hassankhosrojerdi@hassans-MacBook-Pro.local>
2019-09-28 09:21:44 +0330

    part 11

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,15 @@
 |
 */
 
-Route::get('/' , 'HomeController@index');
+// Route::get('/category' , 'CategoryController@index');
+// Route::get('/category/create' , 'CategoryController@create');
+// Route::post('/category' , 'CategoryController@store');
+// Route::get('/category/show/{id}' , 'CategoryController@show');
+// Route::get('/category/edit/{id}' , 'CategoryController@edit');
+// Route::put('/category/update/{id}' , 'CategoryController@update');
+// Route::delete('/category/destroy/{id}' , 'CategoryController@destroy');
+
+Route::resource('/category', 'CategoryController');
+
+
 
