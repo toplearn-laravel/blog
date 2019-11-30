bb00d028bc0888faa5f340325f37f53f7620223b
hassan khosrojerdi <hassankhosrojerdi@hassans-MacBook-Pro.local>
2019-11-30 13:03:29 +0330

    part-21

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,28 @@
 
 @section('content')
   Hi this is a test content
+  {{--  @include('error')  --}}
+  {{--  @includeIf('error')  --}}
+  {{--  @includeWhen($boolean,'error', ['some' => 'data'] )  --}}
+
+
+
+  @if (count($projects) > 0)
+  <ul>
+      @foreach ($projects as $project)
+      @include('layouts.partials.error' , $project)
+      @endforeach
+  </ul>
+  @else
+  @include('layouts.partials.error.none')
+  @endif
+
+  @each('layouts.partials.error', $projects, 'project', 'layouts.partials.error.none')
+
+
+
+
+
 @endsection
 @section('script')
   <script type="text/javascript" src="test.js"></script>
