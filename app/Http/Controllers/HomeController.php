07b7fda2a5eccbbf7d504c678dad9cfd42544747
Hassan <hassankhosrojerdi@yahoo.com>
2020-02-17 00:19:11 +0330

    part-37

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Post;
+use DB;
+
 
 class HomeController extends Controller
 {
     public function index()
     {
-      $user = factory(Post::class, 10)->create();
+      // $users = DB::select('select * from users where id = ?', [$id]);
+      // $users = DB::select('select * from users where id = :id', ['id' => 2]);
+      // $users = DB::insert('insert into users (name, email, password) values (?, ?, ?)', ['hassan', 'hassan@yahoo.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
+      // $users = DB::update('update users set name = ? where id = ?', ['ali', 65]);
+      // $users = DB::delete('delete from users where id = ?', [65]);
         return view('welcome');
     }
 }
