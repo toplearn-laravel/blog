ec01fa51a421e62c94b6ea9bc06cd11210eb37af
Hassan <hassankhosrojerdi@yahoo.com>
2020-03-04 12:21:51 +0330

    part-40

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,20 +12,19 @@ class HomeController extends Controller
 {
     public function index()
     {
-    //    $users = DB::table('users')->whereBetween('id', [1,10])->get();
-    //    $users = DB::table('users')->whereNotBetween('id', [1,10])->get();
-    //    $users = DB::table('users')->whereIn('id', [1, 10, 55, 33])->get();
-    //    $users = DB::table('users')->whereNotIn('id', [1, 10, 55, 33])->get();
-    //    $users = DB::table('users')->whereNull('updated_at')->get();
-    //    $users = DB::table('users')->whereNotNull('updated_at')->get();
-    //    $users = DB::table('users')->whereDate('updated_at', '2020-02-15')->get();
-    //    $users = DB::table('users')->whereColumn(['name', '<>', 'email'])->get();
-    //    $users = DB::table('users')->where('id', 1)->orWhere('id', 2)->get();
-       $users = DB::table('users')->where('id', 1)->where(function ($query){
-           $query->where('name', 'آرام کاشی')->orWhere('email', 'pebrahimi@example.com');
-       })->get();
 
-    //    select * from users where id = 1 and (name = آرام کاشی Or email = pebrahimi@example.com)
+
+        // $users = DB::table('posts')->join('users', 'users.id', 'posts.user_id')->get();
+        // $first =  DB::table('users')->whereNull('updated_at');
+
+        // $users = DB::table('users')->orderBy('id', 'asc')->get();
+        // $users = DB::table('users')->latest()->first();
+        // $users = DB::table('users')->inRandomOrder()->first();
+        // $users = DB::table('users')->take(5)->get();
+        // $users = DB::table('users')->skip(10)->take(5)->get();
+        // $users = DB::table('users')->offset(10)->limit(5)->get();
+
+        // $users = DB::table('posts')->groupBy('user_id')->select('user_id', DB::raw('count(*) as total'))->get();
 
        dd($users);
         return view('welcome');
