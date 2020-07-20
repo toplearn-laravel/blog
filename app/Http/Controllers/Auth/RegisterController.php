ca5243bb8dfc151fd20926f4c1c1982036f64d64
Hassan <hassankhosrojerdi@yahoo.com>
2020-07-20 15:41:02 +0430

    part-82

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -63,7 +63,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-danger">
                                     {{ __('Register') }}
                                 </button>
                             </div>
