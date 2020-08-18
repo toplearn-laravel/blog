0cd71d6ab48546237ea38ca1993b37f15717f077
Hassan <hassankhosrojerdi@yahoo.com>
2020-08-18 19:31:53 +0430

    part-96

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>تاپ لرن</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <style>
+            body{
+                text-align: right
+            }
+        th , tr {
+            text-align: right;
+        }
+    </style>
+</head>
+<body dir="rtl">
+
+    <div class="container">
+
+        @yield('content')
+
+    </div>
+
+</body>
+</html>
