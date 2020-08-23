58156a15a4389ea1d0bd3d64c8661da203ad14aa
Hassan <hassankhosrojerdi@yahoo.com>
2020-08-23 19:42:02 +0430

    part-98

--- tests/Unit/FirstTest.php
+++ tests/Unit/FirstTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,16 +14,37 @@ class firstTest extends TestCase
      *
      * @return void
      */
-    // public function test_input_missing_a_title_is_rejected()
-    // {
-    //   $response = $this->post(route('post.store'), ['title' => 'test title']);
-    //   $response->assertRedirect();
-    //   $response->assertSessionHasErrors();
-    // }
 
-    // public function test_valid_input_should_create_a_post_in_the_database()
-    // {
-    //     $this->post(route('post.store'), ['title' => 'title2', 'user_id' => 295]);
-    //     $this->assertDatabaseHas('posts', ['title' => 'title2', 'user_id' => 295]);
-    // }
+
+
+     public function test_it_creates_a_new_post(){
+
+    //     $user = factory(User::class)->create();
+    //     $this->be($user);
+    //     $this->post(route('post.store'),
+    //     [
+    //         'title' => 'test100000',
+    //         'user_id' => 200,
+    //     ]);
+
+    //     $this->assertDatabaseHas('posts',[
+    //         'title' => 'test100000',
+    //         'user_id' => 200,
+    //     ]);
+    //  }
+
+        $user = factory(User::class)->create();
+        $this->actingAs($user)->post(route('post.store'),
+        [
+            'title' => 'test100000',
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertDatabaseHas('posts',[
+            'title' => 'test100000',
+            'user_id' => $user->id,
+        ]);
+     }
+
+
 }
